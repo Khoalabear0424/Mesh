ee9721a7283318c4a3e91ceb5b38a4e22d54a0ac
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-03 21:22:30 -0700

    set up input validation with express validator

--- server.js
+++ server.js
@@ -9,6 +9,9 @@ app.get('/', (req, res) => {
     res.send('API Running...')
 })
 
+//Init Middleware
+app.use(express.json({ extended: false }));
+
 //Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
