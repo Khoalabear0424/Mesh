d4b62a2314a4420a54db592b314227cb05aea681
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-03 21:02:25 -0700

    added routes to server

---
+++ routes/api/posts.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// @route GET api/post
+// @desc Test Route
+// @access Public
+router.get('/', (req, res) => {
+    res.send('Post route')
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -5,8 +5,20 @@ const connectDB = require('./config/db');
 //Connect database
 connectDB();
 
+app.get('/', (req, res) => {
+    res.send('API Running...')
+})
+
+//Define Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/profile', require('./routes/api/profile'));
+app.use('/api/posts', require('./routes/api/posts'));
+
+
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-    console.log(`Litening on port ${PORT}...`)
+    console.log(`Listening on port ${PORT}...`)
 });
