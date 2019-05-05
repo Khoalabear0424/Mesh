cf25b438d45dcbf5bee38e07a91c34b8a922aa8b
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-04 21:57:53 -0700

    changed some stuff

--- server.js
+++ server.js
@@ -18,8 +18,6 @@ app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/profile', require('./routes/api/profile'));
 app.use('/api/posts', require('./routes/api/posts'));
 
-
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
