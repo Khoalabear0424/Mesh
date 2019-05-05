df44b1ef9ee4ffe078cdff3238548794415a5998
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-04 22:52:17 -0700

    user registration before jwt

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 const { check, validationResult } = require('express-validator/check')
 
 const User = require('../../models/User');
@@ -26,7 +27,7 @@ router.post('/', [
             let user = await User.findOne({ email });
 
             if (user) {
-                res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+                return res.status(400).json({ errors: [{ msg: 'User already exists' }] });
             }
 
             // Get users gravatar
@@ -41,13 +42,18 @@ router.post('/', [
                 email,
                 avatar,
                 password
-            })
+            });
 
             // Encrypt password
+            const salt = await bcrypt.genSalt(10);
+
+            user.password = await bcrypt.hash(password, salt);
+
+            await user.save();
+
 
             // Return jsonwebtoken
-            res.send('User route');
-
+            res.send('User registered');
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server Error');
