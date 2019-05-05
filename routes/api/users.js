a4453a043e8cda25d26ce797fa389828f2b8675f
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-05 16:50:52 -0700

    set up secret and token

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator/check')
 
 const User = require('../../models/User');
@@ -53,7 +55,20 @@ router.post('/', [
 
 
             // Return jsonwebtoken
-            res.send('User registered');
+            const paylod = {
+                user: {
+                    id: user.id
+                }
+            }
+
+            jwt.sign(
+                payload,
+                config.get('jwtToken'),
+                { expiresIn: 360000 },
+                (err, token) => {
+                    if (err) throw err;
+                    res.json({ token });
+                });
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server Error');
