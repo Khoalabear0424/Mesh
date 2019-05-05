1a762597a9fac820601bc83cf3b8281f6e9bed92
Khoa Nguyen <37357006+Khoalabear0424@users.noreply.github.com>
2019-05-04 22:44:59 -0700

    building out user regristraiong

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const gravatar = require('gravatar');
 const { check, validationResult } = require('express-validator/check')
+
+const User = require('../../models/User');
 
 // @route     POST api/users
 // @desc      Register User
@@ -10,21 +13,45 @@ router.post('/', [
     check('email', 'Please include valid email').isEmail(),
     check('password', 'Please enter password with 6 or more characters').isLength({ min: 6 })
 ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
 
-        // See if user exists
+        const { name, email, password } = req.body;
 
-        // Get users gravatar
+        try {
+            // See if user exists
+            let user = await User.findOne({ email });
 
-        // Encrypt password
+            if (user) {
+                res.status(400).json({ errors: [{ msg: 'User already exists' }] });
+            }
 
-        // Return jsonwebtoken
+            // Get users gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'mm'
+            })
 
-        res.send('User route');
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            })
+
+            // Encrypt password
+
+            // Return jsonwebtoken
+            res.send('User route');
+
+        } catch (err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
     });
 
 module.exports = router;
